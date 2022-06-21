63424d275359c973cae94d0084fa6d15b9fc3977
jimmyneutront <w8250693648@protonmail.ch>
2022-06-21 14:56:07 -0400

    Document BlockchainExceptionNotifiable

--- src/main/kotlin/com/commuto/interfacedesktop/blockchain/BlockchainExceptionNotifiable.kt
+++ src/main/kotlin/com/commuto/interfacedesktop/blockchain/BlockchainExceptionNotifiable.kt
@@ -2,7 +2,19 @@ package com.commuto.interfacedesktop.blockchain
 
 import javax.inject.Singleton
 
+/**
+ * An interface that a class must implement in order to be notified of [Exception]s encountered by
+ * [BlockchainService].
+ */
 @Singleton
 interface BlockchainExceptionNotifiable {
+    /**
+     * The method called by [BlockchainService] in order to notify the class implementing this
+     * interface of an [Exception] encountered by [BlockchainService].
+     *
+     * @param exception The [Exception] encountered by [BlockchainService] of which the class
+     * implementing this interface is being notified and should handle in the implementation of this
+     * method.
+     */
     fun handleBlockchainException(exception: Exception)
 }
