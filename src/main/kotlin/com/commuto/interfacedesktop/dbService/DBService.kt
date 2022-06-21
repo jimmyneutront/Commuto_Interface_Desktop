0b9fba29ea1f11c2ac92a77fead0e707a9a2b7ed
jimmyneutront <w8250693648@protonmail.ch>
2022-06-21 17:27:15 -0400

    Update DBService documentation

--- src/main/kotlin/com/commuto/interfacedesktop/dbService/DBService.kt
+++ src/main/kotlin/com/commuto/interfacedesktop/dbService/DBService.kt
@@ -6,25 +6,28 @@ import com.commuto.interfacedesktop.db.KeyPair
 import com.commuto.interfacedesktop.db.PublicKey
 
 /**
- * The Database Service Class
+ * The Database Service Class.
  *
- * This is responsible for storing data, serving it to Local Services upon request, and accepting Local Services'
- * requests to add and remove data from storage.
+ * This is responsible for storing data, serving it to other services upon request, and accepting services' requests to
+ * add and remove data from storage.
  *
  * @property databaseDriverFactory the DatabaseDriverFactory that DBService will use to interact with the database.
+ * @property database The [Database] holding Commuto Interface data.
  */
 class DBService(val databaseDriverFactory: DatabaseDriverFactory) {
+
     private val database = Database(databaseDriverFactory)
+
     //TODO: Localize error strings
     /**
-     * Creates all necessary tables for proper operation.
+     * Creates all necessary database tables.
      */
     fun createTables() {
         database.createTables()
     }
 
     /**
-     * Clears entire database
+     * Clears the entire [database].
      */
     fun clearDatabase() {
         database.clearDatabase()
@@ -33,11 +36,9 @@ class DBService(val databaseDriverFactory: DatabaseDriverFactory) {
     /**
      * Persistently stores a key pair associated with an interface id.
      *
-     * @param interfaceId the interface id of the keypair as a byte array encoded to a hexadecimal String
-     * @param publicKey the public key of the keypair as a byte array encoded to a hexadecimal String
-     * @param privateKey the private key of the keypair as a byte array encoded to a hexadecimal String
-     *
-     * Used exclusively by KMService
+     * @param interfaceId The interface ID of the key pair as a byte array encoded to a hexadecimal [String].
+     * @param publicKey The public key of the key pair as a byte array encoded to a hexadecimal [String].
+     * @param privateKey The private key of the key pair as a byte array encoded to a hexadecimal [String].
      */
     fun storeKeyPair(interfaceId: String, publicKey: String, privateKey: String) {
         val keyPair = KeyPair(interfaceId, publicKey, privateKey)
@@ -50,16 +51,16 @@ class DBService(val databaseDriverFactory: DatabaseDriverFactory) {
     }
 
     /**
-     * Retrieves the persistently stored key pair associated with the given interface id, or null if not present.
+     * Retrieves the persistently stored key pair associated with the given interface ID, or returns null if no such key
+     * pair is present.
      *
-     * @param interfaceId the interface id of the keypair as a byte array encoded to a hexadecimal String
+     * @param interfaceId The interface ID of the desired key pair as a byte array encoded to a hexadecimal [String].
      *
-     * @return KeyPair object (defined in com.commuto.interfacedesktop.db) if key pair is found
-     * @return null if key pair is not found
-     * @throws IllegalStateException if multiple key pairs are found for a given interface id, or if the interface id
-     * returned from the database query does not match the interface id passed to getKeyPair
+     * @return A [KeyPair] if key pair with the specified interface ID is found, or null if such a key pair is not
+     * found.
      *
-     * Used exclusively by KMService
+     * @throws IllegalStateException if multiple key pairs are found for a single interface ID, or if the interface ID
+     * of the key pair returned from the database query does not match [interfaceId].
      */
     fun getKeyPair(interfaceId: String): KeyPair? {
         val dbKeyPairs: List<KeyPair> = database.selectKeyPairByInterfaceId(interfaceId)
@@ -77,12 +78,12 @@ class DBService(val databaseDriverFactory: DatabaseDriverFactory) {
     }
 
     /**
-     * Persistently stores a public key associated with an interface id.
+     * Persistently stores a public key associated with an interface ID.
      *
-     * @param interfaceId the interface id of the keypair as a byte array encoded to a hexadecimal String
-     * @param publicKey the public key to be stored as a byte array encoded to a hexadecimal String
+     * @param interfaceId The interface ID of the key pair as a byte array encoded to a hexadecimal [String].
+     * @param publicKey The public key to be stored, as a byte array encoded to a hexadecimal [String].
      *
-     * @throws IllegalStateException if a public key with the given interface id is already found in the database
+     * @throws IllegalStateException if a public key with the given interface ID is already found in the database.
      */
     fun storePublicKey(interfaceId: String, publicKey: String) {
         val publicKey = PublicKey(interfaceId, publicKey)
@@ -95,16 +96,16 @@ class DBService(val databaseDriverFactory: DatabaseDriverFactory) {
     }
 
     /**
-     * Retrieves the persistently stored public key associated with the given interface id, or null if not present
+     * Retrieves the persistently stored public key associated with the given interface ID, or returns nul if no such
+     * key is found.
      *
-     * @param interfaceId the interface id of the public key as a byte array encoded to a hexadecimal String
+     * @param interfaceId The interface ID of the public key as a byte array encoded to a hexadecimal [String].
      *
-     * @return PublicKey object (definded in com.commuto.interfacedesktop.db) if public key is found
-     * @return null if public key is not found
-     * @throws IllegalStateException if multiple public keys are found for a given interface id, or if the interface id
-     * returned from the database query does not match the interface id passed to getPublicKey
+     * @return A [PublicKey] if a public key with the specified interface ID is found, or null if no such public key is
+     * found.
      *
-     * Used exclusively by KMService
+     * @throws IllegalStateException if multiple public keys are found for a given interface ID, or if the interface ID
+     * of the public key returned from the database query does not match [interfaceId].
      */
     fun getPublicKey(interfaceId: String): PublicKey? {
         val dbPublicKeys: List<PublicKey> = database.selectPublicKeyByInterfaceId(interfaceId)
