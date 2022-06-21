d5a380eb322a8e034a622c0c3af5d1f5cb67a4b4
jimmyneutront <w8250693648@protonmail.ch>
2022-06-21 15:12:21 -0400

    Document BlockchainExceptionNotifiableModule

--- src/main/kotlin/com/commuto/interfacedesktop/blockchain/BlockchainExceptionNotifiableModule.kt
+++ src/main/kotlin/com/commuto/interfacedesktop/blockchain/BlockchainExceptionNotifiableModule.kt
@@ -4,8 +4,19 @@ import com.commuto.interfacedesktop.ui.ExceptionViewModel
 import dagger.Binds
 import dagger.Module
 
+/**
+ * A Dagger [Module] that tells Dagger what to inject into objects that depend on an object
+ * implementing [BlockchainExceptionNotifiable]
+ */
 @Module
 interface BlockchainExceptionNotifiableModule {
+    /**
+     * A Dagger Binding that tells Dagger to inject an instance of [ExceptionViewModel] into objects
+     * that depend on an object implementing [BlockchainExceptionNotifiable].
+     *
+     * @param impl The type of object that will be injected into objects that depend on an object
+     * implementing [BlockchainExceptionNotifiable].
+     */
     @Binds
     fun bindBlockchainExceptionNotifiable(impl: ExceptionViewModel): BlockchainExceptionNotifiable
 }
