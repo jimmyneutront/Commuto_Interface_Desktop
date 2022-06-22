74a88f9e7386add12d471de90ca416ae79a52b03
jimmyneutront <w8250693648@protonmail.ch>
2022-06-21 23:56:08 -0400

    Document main function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,11 @@
 
 import com.commuto.interfacedesktop.DaggerCommutoApplicationFactory
 
+/**
+ * The main entry point for the Commuto Interface Desktop app. This creates a
+ * [com.commuto.interfacedesktop.CommutoApplication] factory, gets an instance from the factory and calls the instance's
+ * start method to start the app.
+ */
 fun main() {
     val appFactory = DaggerCommutoApplicationFactory.create()
     val app = appFactory.commutoApplicationFactory()
