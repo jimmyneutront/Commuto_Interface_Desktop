2f270b86c181cddee55a2c4eefcbab017707f134
jimmyneutront <w8250693648@protonmail.ch>
2022-06-21 23:45:15 -0400

    Document OffersViewModel

--- src/main/kotlin/com/commuto/interfacedesktop/ui/OffersViewModel.kt
+++ src/main/kotlin/com/commuto/interfacedesktop/ui/OffersViewModel.kt
@@ -4,5 +4,13 @@ import com.commuto.interfacedesktop.offer.OfferService
 import javax.inject.Inject
 import javax.inject.Singleton
 
+/**
+ * The Offers View Model, the single source of truth for all offer-related data. It is observed by
+ * offer-related [androidx.compose.runtime.Composable]s.
+ *
+ * @property offerService The [OfferService] responsible for adding and removing
+ * [com.commuto.interfacedesktop.offer.Offer]s from the list of open offers as they are
+ * created, canceled and taken.
+ */
 @Singleton
 class OffersViewModel @Inject constructor(val offerService: OfferService)
