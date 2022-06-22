fef06584c2a9fdeb2b4d01e77409e309386b4d80
jimmyneutront <w8250693648@protonmail.ch>
2022-06-21 22:39:21 -0400

    Document OfferNotifiableModule

--- src/main/kotlin/com/commuto/interfacedesktop/offer/OfferNotifiableModule.kt
+++ src/main/kotlin/com/commuto/interfacedesktop/offer/OfferNotifiableModule.kt
@@ -3,8 +3,19 @@ package com.commuto.interfacedesktop.offer
 import dagger.Binds
 import dagger.Module
 
+/**
+ * A Dagger [Module] that tells Dagger what to inject into objects that depend on an object
+ * implementing [OfferNotifiable]
+ */
 @Module
 interface OfferNotifiableModule {
+    /**
+     * A Dagger Binding that tells Dagger to inject an instance of [OfferService] into objects
+     * that depend on an object implementing [OfferNotifiable].
+     *
+     * @param impl The type of object that will be injected into objects that depend on an object
+     * implementing [OfferNotifiable].
+     */
     @Binds
     fun bindOfferNotifiable(impl: OfferService): OfferNotifiable
 }
