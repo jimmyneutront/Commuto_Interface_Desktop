58e46c01653a79909ad8764b9f31ab65e306f916
jimmyneutront <w8250693648@protonmail.ch>
2022-06-21 23:17:11 -0400

    Document ExceptionViewModel

--- src/main/kotlin/com/commuto/interfacedesktop/ui/ExceptionViewModel.kt
+++ src/main/kotlin/com/commuto/interfacedesktop/ui/ExceptionViewModel.kt
@@ -4,8 +4,19 @@ import com.commuto.interfacedesktop.blockchain.BlockchainExceptionNotifiable
 import javax.inject.Inject
 import javax.inject.Singleton
 
+/**
+ * The Exception View Model, used to display a visual notification to the user when unexpected
+ * exceptions occur.
+ */
 @Singleton
 class ExceptionViewModel @Inject constructor(): BlockchainExceptionNotifiable {
+    /**
+     * The method called by [com.commuto.interfacedesktop.blockchain.BlockchainService] in order to notify of an
+     * [Exception] encountered by [com.commuto.interfacedesktop.blockchain.BlockchainService].
+     *
+     * @param exception The [Exception] encountered by [com.commuto.interfacedesktop.blockchain.BlockchainService] about
+     * which this method should notify the user.
+     */
     override fun handleBlockchainException(exception: Exception) {
         throw exception
     }
