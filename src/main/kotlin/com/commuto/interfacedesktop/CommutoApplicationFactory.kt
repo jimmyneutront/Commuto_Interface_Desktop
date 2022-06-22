28ff2d4772f466e0aae6e47c1f264e1912d00bc2
jimmyneutront <w8250693648@protonmail.ch>
2022-06-21 23:53:46 -0400

    Document CommutoApplicationFactory

--- src/main/kotlin/com/commuto/interfacedesktop/CommutoApplicationFactory.kt
+++ src/main/kotlin/com/commuto/interfacedesktop/CommutoApplicationFactory.kt
@@ -6,10 +6,17 @@ import dagger.Component
 import javax.inject.Singleton
 
 // Note: Dagger causes building to fail if the last element in the modules list has a trailing comma
+/**
+ * A [CommutoApplication] factory, required by Dagger in order to automatically generate the
+ * [DaggerCommutoApplicationFactory] class.
+ */
 @Singleton
 @Component(modules = [OfferNotifiableModule::class,
     BlockchainExceptionNotifiableModule::class
 ])
 interface CommutoApplicationFactory {
+    /**
+     * Tells Dagger how to generate [CommutoApplicationFactory]'s [commutoApplicationFactory] method.
+     */
     fun commutoApplicationFactory(): CommutoApplication
 }
